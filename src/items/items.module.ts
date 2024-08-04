6dc03c5631183d1bf72e683d60fff92d9fe4a39b
Mohamed Khaled <mohamed.khaled.200540@gmail.com>
2024-08-04 13:32:53 +0300

    adding a subscriber to the database on table item

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -6,10 +6,11 @@ import { ItemsService } from './items.service';
 import { Listing } from './entities/listing.entity';
 import { Comment } from './entities/comment.entity';
 import { Tag } from './entities/tag.entity';
+import { ItemSubscriber } from './entities/item.subscriber';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Item, Listing, Comment, Tag])],
   controllers: [itemsController],
-  providers: [ItemsService]
+  providers: [ItemsService, ItemSubscriber]
 })
 export class ItemsModule {}
